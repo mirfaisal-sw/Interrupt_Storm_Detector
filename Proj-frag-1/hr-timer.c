a74c6fbecdd90a019575bcbc19455098467514fa
MIR FAISAL <80507027+mirfaisal-sw@users.noreply.github.com>
2024-02-06 23:04:56 +0530

    updated 06th Feb

--- Proj-frag-1/hr-timer.c
+++ Proj-frag-1/hr-timer.c
@@ -1,3 +1,8 @@
+/* Reference: - 
+ * 1. https://lwn.net/Articles/167897/
+ * 2. 
+ */
+
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -16,6 +21,11 @@ enum hrtimer_restart my_hrtimer_callback( struct hrtimer *timer )
  
 static int hrt_init_module( void )
 {
+	/* ktime_t, which is used to store a time value in nanoseconds. This type,
+	 * found in <linux/ktime.h>, is meant to be used as an opaque structure. And, 
+	 * interestingly, its definition changes depending on the underlying architecture. 
+	 * On 64-bit systems, a ktime_t is really just a 64-bit integer value in nanoseconds.
+	 */
     ktime_t ktime;
     unsigned long delay_in_ms = 200L;
 
@@ -25,14 +35,14 @@ static int hrt_init_module( void )
      * ktime = ktime_set(0, 200 * 1000 * 1000);
      * 200 ms = 10 * 1000 * 1000 ns
      */
-    ktime = ktime_set( 0, MS_TO_NS(delay_in_ms) );
+    ktime = ktime_set(0, MS_TO_NS(delay_in_ms) );
 
-    hrtimer_init( &hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL );
+    hrtimer_init(&hr_timer, CLOCK_MONOTONIC, HRTIMER_MODE_REL);
     hr_timer.function = &my_hrtimer_callback;
-    pr_info( "Starting timer to fire in %ldms (%ld)\n", \
+    pr_info("Starting timer to fire in %ldms (%ld)\n", \
            delay_in_ms, jiffies );
 
-    hrtimer_start( &hr_timer, ktime, HRTIMER_MODE_REL );
+    hrtimer_start(&hr_timer, ktime, HRTIMER_MODE_REL);
     return 0;
 }
  
@@ -49,6 +59,7 @@ static void hrt_cleanup_module( void )
 
 module_init(hrt_init_module);
 module_exit(hrt_cleanup_module);
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("John Madieu <john.madieu@gmail.com>");
 MODULE_DESCRIPTION("Standard timer example");
